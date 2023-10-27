c0a326d3f8a610da54ae2bf5c51b96b3af48f66c
alexnjoya <njoyaalexander71@gmail.com>
2023-10-27 11:28:51 +0000

    first commit

--- src/scenes/Account/AccountCreate.tsx
+++ src/scenes/Account/AccountCreate.tsx
@@ -67,7 +67,7 @@ function AccountCreate() {
   return (
     <div className='AccountCreate p-5 m-3 card shadow'>
       <h1>
-        Aqua Wallet
+        Wbasa Wallet
       </h1>
       <form onSubmit={event => event.preventDefault()}>
         <button type="button" className="btn btn-primary" onClick={createAccount}>
